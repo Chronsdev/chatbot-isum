ec489aef060702c992d720c522417c73206511e8
Victor <victorarmao16@gmail.com>
2025-11-16 23:07:48 -0400

    cambios nuevos 11

--- run_bot.py
+++ run_bot.py
@@ -3,7 +3,7 @@ import rasa
 from dotenv import load_dotenv
 load_dotenv()
 
-port = os.environ.get("PORT", "5085")
+port = os.environ.get("PORT", "5005")
 
 # Arrancar el servidor con el modelo recién entrenado
 os.system(f"rasa run --enable-api --port {port}")
