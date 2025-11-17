5ce263bdd7a59caf6b105061953f9e28d004ed3d
Victor <victorarmao16@gmail.com>
2025-11-16 20:48:10 -0400

    cambios nuevos 4

--- run_bot.py
+++ run_bot.py
@@ -5,8 +5,5 @@ load_dotenv()
 
 port = os.environ.get("PORT", "5005")
 
-# Entrenar antes de arrancar
-os.system("rasa train")
-
 # Arrancar el servidor con el modelo recién entrenado
-os.system(f"rasa run --enable-api --port {port}")
+rasa.run(enable_api=True, port=port)
